dbdf7682f98f0d9b2566f83ac3833b41d406cd9a
atharwa-pandey <76654647+atharwa-pandey@users.noreply.github.com>
2021-09-09 15:39:54 +0530

    Add files via upload

--- js/contactus.js
+++ js/contactus.js
@@ -126,3 +126,15 @@ let spinnerWrapper = document.querySelector('.spinner-wrapper');
     distance: '100px',
     reset: 'always'
     });
+    window.sr = ScrollReveal();
+    sr.reveal('.rightEntrance' , {
+    duration: 2000,
+    origin:'right',
+    distance: '50px'
+    });
+    window.sr = ScrollReveal();
+    sr.reveal('.leftEntrance' , {
+    duration: 2000,
+    origin:'left',
+    distance: '50px'
+    });
